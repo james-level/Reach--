6c5acdef6b98c53a2d3d2ae90c31d7c1834e51de
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-02 11:38:12 +0100

    adds forms file

---
+++ social/social_reach/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from rango.models import Page, Category
+from rango.models import UserProfile
+from django.contrib.auth.models import User
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Category
+
+        fields = ('name', 'views', 'likes')
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Enter page title.")
+    url = forms.URLField(max_length=128, help_text="Enter URL")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+
+        fields = ('title', 'url', 'views')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('https://'):
+            url = 'https://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+
+        fields = ('username', 'email', 'password')
+
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+
+        fields = ('website', 'picture')
