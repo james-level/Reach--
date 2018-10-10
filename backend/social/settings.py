414c322564854d844957835816c401e35af69194
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-10 09:57:48 +0100

    youtube subscribers scraped on create and update of profile
    Co-authored-by: Stephen Rooney <SFR1981@users.noreply.github.com>
    Co-authored-by: David Pears <DavidAPears@users.noreply.github.com>
    Co-authored by: Shaun Adams <Adams1991@users.noreply.github.com

--- backend/social/settings.py
+++ backend/social/settings.py
@@ -50,7 +50,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
     )
