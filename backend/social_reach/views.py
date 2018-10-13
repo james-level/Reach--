6ab4a5af0b8b6927e613a37ed900e635adc42ec3
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-13 21:25:04 +0100

    deletes extraneous view classes

--- backend/social_reach/views.py
+++ backend/social_reach/views.py
@@ -172,50 +172,6 @@ class UserCreateView(generics.CreateAPIView):
             settings.EMAIL.confirmation(self.request, context).send(to)
 
 
-
-# class UserViewSet(ModelViewSet):
-#     """
-#     A simple ViewSet for viewing and editing accounts.
-#     """
-#     queryset = get_user_model().objects.all()
-#     serializer_class = UserSerializer
-#     permission_classes = [IsAdminUser]
-
-# class VerifyEmailView(APIView):
-#     permission_classes = (AllowAny,)
-#     allowed_methods = ('POST', 'OPTIONS', 'HEAD')
-#
-#     def get_serializer(self, *args, **kwargs):
-#         return VerifyEmailSerializer(*args, **kwargs)
-#
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.kwargs['key'] = serializer.validated_data['key']
-#         try:
-#             confirmation = self.get_object()
-#             confirmation.confirm(self.request)
-#             return Response({'detail': _('Successfully confirmed email.')}, status=status.HTTP_200_OK)
-#         except EmailConfirmation.DoesNotExist:
-#             return Response({'detail': _('Error. Incorrect key.')}, status=status.HTTP_404_NOT_FOUND)
-#
-#     def get_object(self, queryset=None):
-#         key = self.kwargs['key']
-#         emailconfirmation = EmailConfirmationHMAC.from_key(key)
-#         if not emailconfirmation:
-#             if queryset is None:
-#                 queryset = self.get_queryset()
-#             try:
-#                 emailconfirmation = queryset.get(key=key.lower())
-#             except EmailConfirmation.DoesNotExist:
-#                 raise EmailConfirmation.DoesNotExist
-#         return emailconfirmation
-#
-#     def get_queryset(self):
-#         qs = EmailConfirmation.objects.all_valid()
-#         qs = qs.select_related("email_address__user")
-#         return qs
-
 class ListCategoryView(generics.ListCreateAPIView):
     """
     Provides a get method handler.
