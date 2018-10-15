c0f5994bf1c3b8f83dd83f7a9ae35cc3807c7139
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-15 01:43:46 +0100

    can retrieve user details for profile creation

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -14,7 +14,7 @@ class Register extends Component {
         signUpSubmit: false,
         data: {},
         activation_token: '',
-        activation_user: '',
+        activation_user: null,
 
           name: 'test',
           location: '',
@@ -40,6 +40,7 @@ class Register extends Component {
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
 
       };
 
@@ -76,13 +77,18 @@ class Register extends Component {
     componentDidMount(){
 
       // needs updated to include the correct activation components
+      var self = this;
       var uid = this.props.data.match.params.id
       console.log(uid);
       var token = this.props.data.match.params.token
       console.log(token);
       var activation_url = `http://localhost:8080/social_reach/auth/users/confirmation/${uid}/${token}`
        axios.get(`${activation_url}/?format=json`).then(function (response) {
-            console.log(response);
+            console.log(response.data.user);
+            self.setState({
+              activation_user: response.data.user
+            })
+            console.log(self.state.activation_user);
         }).catch(function (error) {
                 console.log(error);
         });
@@ -90,8 +96,12 @@ class Register extends Component {
   }
 
     render(){
+      if (this.state.activation_user){
       return (
         <div className="register">
+
+        <h6 align="center" style={{fontWeight: 'bold'}}>{"Let's start making you a killer profile, "} {this.state.activation_user['username']}</h6>
+<p></p>
 
       {/* PROFILE INFO INPUT FORM START */}
       <div class="user-input-form">
@@ -168,8 +178,89 @@ class Register extends Component {
         </div>
       )
     }
+    else {
+      return (
+        <div className="register">
 
 
-};
+
+      {/* PROFILE INFO INPUT FORM START */}
+      <div class="user-input-form">
+        <form onSubmit={this.handleSubmit}>
+
+      {/* BASIC INFO SECTION */}
+          <fieldset>
+            <legend><span class="number"></span> Basic Info</legend>
+            <input onChange={this.handleChange} type="text" name="name" placeholder="Your Name *"></input>
+            <input type="text" onChange={this.handleChange} name="location" placeholder="The Nearest Town/City To Where You Live *"></input>
+            <input type="date" onChange={this.handleChange} name="date_of_birth" placeholder="Your Date Of Birth *"></input>
+            <p> gender select </p>
+            <input type="range"  onChange={this.handleChange} max="100" min="-100" step="1" name="gender" placeholder="Your Gender *"></input>
+            <textarea name="description" onChange={this.handleChange} placeholder="Description (max 500 characters) *" maxlength="500"></textarea>
+
+      {/*  TODO: Replace this 'Interests drop-down (below) with a 'show emoji's to represent you' field?   */}
+            <label for="job">Interests:</label>
+              <select id="job" name="interests" onChange={this.handleChange}>
+                <optgroup label="I like to post/blog/vlog about...">
+                  <option value="travel">Travel</option>
+                  <option value="ecology">Saving The Planet</option>
+                  <option value="gym">Gym</option>
+                  <option value="animals">Animals</option>
+                  <option value="politics">Politics</option>
+                  <option value="food">Food Porn</option>
+                  <option value="fashion">Fashion/Beauty</option>
+                  <option value="sport">Sport</option>
+                  <option value="music">Music</option>
+                  <option value="films">Films</option>
+                  <option value="geeky stuff">Geeky stuff</option>
+                </optgroup>
+              </select>
+            </fieldset>
+
+
+      {/* SOCIAL MEDIA SECTION */}
+            <fieldset>
+              <legend><span class="number"></span>Social Reach</legend>
+              <input type="text" onChange={this.handleChange} name="twitter_handle" placeholder="Twitter         (enter the bit after 'twitter.com/') "></input>
+              <input type="text" onChange={this.handleChange} name="instagram_handle" placeholder="Instagram   (enter the bit after 'instagram.com/') "></input>
+              <input type="text" onChange={this.handleChange} name="youtube_handle" placeholder="YouTube     (enter the bit after 'youtube.com/user/') "></input>
+              <input type="text" onChange={this.handleChange} name="facebook_handle" placeholder="Facebook     (enter the bit after 'facebook.com/') "></input>
+              <input type="text" onChange={this.handleChange} name="snapchat" placeholder="SnapChat     (enter the bit after 'snapchat.com') "></input>
+              <input type="text" onChange={this.handleChange} name="spotify_handle" placeholder="Spotify    (ARTISTS ONLY!)"></input>
+            </fieldset>
+
+
+      {/* OTHER INFO ECTION */}
+        <fieldset>
+          <legend><span class="number"></span>Additional Info</legend>
+          <textarea name="additional_info" onChange={this.handleChange} placeholder="Anything else you want to tell the world?" maxlength="120"></textarea>
+        </fieldset>
+
+      {/* PHOTO UPLOAD SECTION */}
+        <fieldset>
+          <legend><span class="number"></span>Photos</legend>
+          <input type="file" onChange={this.handleChange} name="photo1" class="foto-upload"></input>
+        </fieldset>
+
+      {/*  SAVE BUTTON */}
+        <br></br>
+          <input type="submit" name="field12" class="Save"></input>
+
+        </form>
+      </div>
+
+
+      {/* PROFILE INFO INPUT FORM END */}
+
+
+
+        </div>
+      )
+      }
+
+
+    }
+
+}
 
 export default Register;
