9069080ad17fb51ce6a456cdaafb4c55a3c25495
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-17 15:40:44 +0100

    total reach breakdown chart rendering everywhere, undesired

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import $ from 'jquery';
 
 
 class Profile extends Component {
@@ -12,18 +13,30 @@ class Profile extends Component {
     };
   }
 
+  total_reach(){
+    return this.props.data.instagram_followers + this.props.data.twitter_followers + this.props.data.youtube_followers
+  }
+
+
   render(){
     console.log(this.props);
 //ternary to either display profile or log in message
   const post = this.props.loggedInAs  ? (
 
                     <div className="profile">
+                     <script type="text/javascript" src="reach_pie_chart.js"></script>
+                     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 
-                    <img className="landing-image" src="/images/app_images/user-profile-icon.png" alt="user-profile-icon"></img>
+
+                    <img className="landing-image" src="/images/app_images/user-profile-icon.png" alt="user-profile-icon" ></img>
 
                     <h2> {this.props.loggedInAs}, born on {this.props.data.date_of_birth}, gender identity {this.props.data.gender_identity}</h2>
 
                     <h4> {this.props.data.bio}</h4>
+
+                    <h4> Total Reach: {this.total_reach()}</h4>
+
+                    <div id="doughnutChart" class="chart"></div>
 
                     </div>
 
@@ -39,5 +52,11 @@ class Profile extends Component {
     )
   }
 }
+
+
+
+
+
+
 
 export default Profile;
