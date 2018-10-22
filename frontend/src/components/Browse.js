88a18752ddc6b673105bd9080330401aad4ab363
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-22 16:52:08 +0100

    desktop swiping feature ultimately abandoned owing to limited options

--- frontend/src/components/Browse.js
+++ frontend/src/components/Browse.js
@@ -1,18 +1,1 @@
-import Cards, { Card } from 'react-swipe-card'
-import React from "react";
 
-const data = ['Alexandre', 'Thomas', 'Lucien']
-
-const Wrapper = () => {
-  return (
-	  <Cards onEnd={action('end')} className='master-root'>
-        {data.map(item =>
-          <Card
-            onSwipeLeft={action('swipe left')}
-            onSwipeRight={action('swipe right')}>
-            <h2>{item}</h2>
-          </Card>
-        )}
-      </Cards>
-  )
-}
