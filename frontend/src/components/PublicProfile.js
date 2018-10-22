c5cdc3b440ebdbede7a7722d8c504f8d2e7c6fea
David A Pears <davidapears@gmail.com>
2018-10-22 21:33:51 +0100

    Adds carousel to 'public profile' page also

--- frontend/src/components/PublicProfile.js
+++ frontend/src/components/PublicProfile.js
@@ -14,6 +14,13 @@ class PublicProfile extends Component {
   }
 
   render(){
+
+    const imageStyle = {backgroundImage: `url(${this.props.data.picture})`}
+    const imageStyle2 = {backgroundSize: "cover", backgroundImage: `url(${this.props.data.picture_two})`}
+    const imageStyle3 = {backgroundImage: `url(${this.props.data.picture_three})`}
+    const imageStyle4 = {backgroundImage: `url(${this.props.data.picture_four})`}
+    const imageStyle5 = {backgroundImage: `url(${this.props.data.picture_five})`}
+    const imageStyle6 = {backgroundImage: `url(${this.props.data.picture_six})`}
     console.log(this.props);
       const post = this.props.loggedInAs  ? (
 
@@ -39,14 +46,34 @@ class PublicProfile extends Component {
                       {/* Photo Carousel */}
                       <fieldset>
                         <legend><span class="number"></span> Selfie Centre </legend>
-                      <div class="gallery" data-flickity='{ "cellAlign": "left", "contain": true }'>
-                        <img class="gallery-cell" src="../images/app_images/user1.jpeg"></img>
-                        <img class="gallery-cell" src="../images/app_images/user2.jpeg"></img>
-                        <img class="gallery-cell" src="../images/app_images/user3.jpeg"></img>
-                        <img class="gallery-cell" src="../images/app_images/user 4.jpeg"></img>
-                        <img class="gallery-cell" src="../images/app_images/user 5.jpeg"></img>
-                        <img class="gallery-cell" src="../images/app_images/user6.jpeg"></img>
-                      </div>
+                        <div class="slider-container">
+                          <div class="slider-menu"> */}
+                            {/* <label for="slide-dot-1"></label> */}
+                            <label for="slide-dot-2"></label>
+                            <label for="slide-dot-3"></label>
+                            <label for="slide-dot-4"></label>
+                            <label for="slide-dot-5"></label>
+                            <label for="slide-dot-6"></label>
+                          </div>
+
+                          {/* <input id="slide-dot-1" type="radio" name="slides" checked></input>
+                          <div class="slide slide-1" style={imageStyle}></div> */}
+
+                          /* <input id="slide-dot-2" type="radio" name="slides"></input>
+                          <div class="slide slide-2" style={imageStyle2}></div>
+
+                          <input id="slide-dot-3" type="radio" name="slides"></input>
+                          <div class="slide slide-3" style={imageStyle3}></div>
+
+                          <input id="slide-dot-4" type="radio" name="slides"></input>
+                          <div class="slide slide-4" style={imageStyle4}></div>
+
+                          <input id="slide-dot-5" type="radio" name="slides"></input>
+                          <div class="slide slide-5" style={imageStyle5}></div>
+
+                          <input id="slide-dot-6" type="radio" name="slides"></input>
+                          <div class="slide slide-6" style={imageStyle6}></div>
+                        </div>
                       </fieldset>
 
                     </div>
