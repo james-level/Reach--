cbc38005b04e7f83ba3a35a3c5657a539f8308f6
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-22 16:09:51 +0100

    removes non-functioning carousel swiper

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -5,7 +5,7 @@ import Register from "./Register";
 import Profile from "./Profile";
 import PublicProfile from "./PublicProfile";
 import SearchUsers from "./SearchUsers";
-import Browse from "./Browse";
+import Carousel from "./Browse";
 import PasswordReset from "./PasswordReset";
 import axios from 'axios';
 import { BrowserRouter as Router, Route} from "react-router-dom";
@@ -272,7 +272,7 @@ console.log("Error resetting password");
                 <Route exact path="/reset_password/:id/:token" render={(props) => <PasswordReset {...props} handlePasswordResetSubmit = {this.handlePasswordResetSubmit} get_uniqueID = {this.get_uniqueID} get_reset_token = {this.get_reset_token} data={props}/>}/>
                 <Route path="/Profile" render={(props) =>  <Profile data={this.state.data} loggedInAs={this.state.loggedInAs} />} />
                 <Route path="/publicProfile" render={(props) =>  <PublicProfile data={this.state.data} loggedInAs={this.state.loggedInAs} />} />
-                <Route path="/browse" render={(props) =>  <Browse data={this.state.data} loggedInAs={this.state.loggedInAs} />} />
+                <Route path="/browse" render={(props) =>  <Carousel data={this.state.data} loggedInAs={this.state.loggedInAs} />} />
                 <Route path="/reachout" render={(props) =>  <SearchUsers data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
 
 
@@ -309,7 +309,7 @@ console.log("Error resetting password");
           <Route exact path="/reset_password/:id/:token" render={(props) => <PasswordReset {...props} handlePasswordResetSubmit = {this.handlePasswordResetSubmit} get_uniqueID = {this.get_uniqueID} get_reset_token = {this.get_reset_token} data={props}/>}/>
           <Route path="/Profile" render={(props) =>  <Profile data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
           <Route path="/publicprofile" render={(props) =>  <Profile data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
-          <Route path="/browse" render={(props) =>  <Browse/>} />
+          <Route path="/browse" render={(props) =>  <Carousel/>} />
           <Route path="/reachout" render={(props) =>  <SearchUsers data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
 
         </React.Fragment>
