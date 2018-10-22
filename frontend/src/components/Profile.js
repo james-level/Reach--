ceef5f6e86aa2b80c7f107d0efa73a8fa737b512
David A Pears <davidapears@gmail.com>
2018-10-22 15:32:03 +0100

    User 'Reach' piechart showing on profile page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -15,6 +15,17 @@ class Profile extends Component {
     };
   }
 
+twitter_followers(){
+  return this.props.data.twitter_followers;
+}
+
+instagram_followers(){
+  return this.props.data.instagram_followers;
+}
+
+youtube_followers(){
+  return this.props.data.youtube_followers;
+}
 
 // REACH DONUT DISPLAY GRAPHIC START
 chart(instagram_reach, twitter_reach, youtube_reach){
@@ -292,9 +303,9 @@ chart(instagram_reach, twitter_reach, youtube_reach){
     var age = getAge(this.props.data.date_of_birth);
     console.log("age:",age);
     console.log(this.props);
-    //   if (this.state.chart == false){
-    //   this.chart(this.props.data.instagram_followers, this.props.data.twitter_followers, this.props.data.youtube_followers)
-    // }
+      if (this.state.chart == false){
+      this.chart(this.props.data.instagram_followers, this.props.data.twitter_followers, this.props.data.youtube_followers)
+    }
 
 
 //ternary to either display profile or log in message
@@ -324,6 +335,42 @@ chart(instagram_reach, twitter_reach, youtube_reach){
         <fieldset>
           <legend><span class="number"></span>Reach: {this.total_reach()} </legend>
         </fieldset>
+
+
+
+
+
+{/* PIE CHART START  */}
+  <div class="donut-chart-block block">
+
+  <div class="donut-chart">
+    <div id="porcion1" class="recorte"><div class="quesito twitter" data-rel={(100/this.total_reach()) * this.twitter_followers()}></div></div>
+    <div id="porcion2" class="recorte"><div class="quesito instagram" data-rel={(100/this.total_reach()) * this.instagram_followers()}></div></div>
+    <div id="porcion3" class="recorte"><div class="quesito youtube" data-rel={(100/this.total_reach()) * this.youtube_followers()}></div></div>
+    <div id="porcionFin" class="recorte"><div class="quesito facebook" data-rel="0"></div></div>
+    <p class="center-date">{this.total_reach()}<br></br><span class="scnd-font-color"></span></p>
+  </div>
+
+    <ul class="os-percentages horizontal-list">
+        <li>
+          <p class="twitter os scnd-font-color">Twitter</p>
+          <p class="os-percentage">{(100/this.total_reach()) * this.twitter_followers()}<sup>%</sup></p>
+        </li>
+        <li>
+          <p class="instagram os scnd-font-color">Instagram</p>
+          <p class="os-percentage">{(100/this.total_reach()) * this.instagram_followers()}<sup>%</sup></p>
+        </li>
+        <li>
+          <p class="youtube os scnd-font-color">YouTube</p>
+          <p class="os-percentage">{(100/this.total_reach()) * this.youtube_followers()}<sup>%</sup></p>
+        </li>
+        <li>
+          <p class="facebook os scnd-font-color">Facebook</p>
+          <p class="os-percentage">0<sup>%</sup></p>
+        </li>
+    </ul>
+  </div>
+{/* PIE CHART END  */}
 
       </div>
 
