376bdc84f780e777b94d841f502b9bcdd0e617a4
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-24 13:32:44 +0100

    add code for PUT request

--- frontend/src/components/Update.js
+++ frontend/src/components/Update.js
@@ -7,13 +7,34 @@ class Update extends Component {
 
   }
 
+  updateReach(){
+
+    var name = this.props.data.name;
+    var bio = this.props.data.bio;
+    var looking_for = this.props.data.looking_for
+    var location = this.props.data.location
+    var date_of_birth = this.props.data.date_of_birth
+    var gender = this.props.data.gender
+    var twitter_handle = this.props.data.twitter_handle
+    var instagram_handle = this.props.data.instagram_handle
+    var youtube_handle = this.props.data.youtube_handle
+    var twitter_followers = this.props.data.twitter_followers
+    var instagram_followers = this.props.data.instagram_followers
+    var youtube_followers = this.props.data.youtube_followers
+    var picture_one = this.props.data.photo1
+    var picture_two = this.props.data.photo2
+    var picture_three = this.props.data.photo3
+    var picture_four = this.props.data.photo4
+    var picture_five = this.props.data.photo5
+    var picture_six = this.props.data.photo6
+
+  }
 
   render(){
-    console.log(this.props);
 
     return(
 
-      <div className="pulsating-circle" >Update My Reach!</div>
+      <div className="pulsating-circle" onClick={this.updateMyReach}>Update My Reach!</div>
 
     )
   }
