000558cbf8ae50a3a896c33e78e1cdfdc9c96641
David A Pears <davidapears@gmail.com>
2018-10-25 13:55:41 +0100

    Gender-selection slider on Registration page styled

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -243,37 +243,13 @@ class Settings extends Component {
 
         {/* DISTANCE RANGE SLIDER  */}
           <fieldset>
-              <p>What kind of distance suits you?</p>
-                <select onChange={this.handleChange} name="miles">
-                  <option disabled hidden value=''></option>
-                  <option value=">5">5 miles</option>
-                  <option value=">10">10 miles</option>
-                  <option value="15">15 miles</option>
-                  <option value="20">20 miles</option>
-                  <option value="25">25 miles</option>
-                  <option value="30">30 miles</option>
-                  <option value="35">35 miles</option>
-                  <option value="40">40 miles</option>
-                  <option value="45">45 miles</option>
-                  <option value="50">50 miles</option>
-                  <option value="55">55 miles</option>
-                  <option value="60">60 miles</option>
-                  <option value="65">65 miles</option>
-                  <option value="70">70 miles</option>
-                  <option value="75">75 miles</option>
-                  <option value="80">80 miles</option>
-                  <option value="85">85 miles</option>
-                  <option value="90">90 miles</option>
-                  <option value="95">95 miles</option>
-                </select>
+              <p>Distance (KMs):</p>
+              <span> <input type="range"  onChange={this.handleChange} max="99" min="0" step="1" name="distance" placeholder="Max Distance (0-100km's) *"></input> </span>
+              <br></br><br></br>
               <input type="submit"  name="fieldb" class="Save"></input>
             </fieldset>
-
-
-
-            </form>
-          </div>
-
+          </form>
+        </div>
       </div>
 
 
