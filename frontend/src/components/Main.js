7464f5b1df29c29f88d9542ebc8057bd90618f13
David A Pears <davidapears@gmail.com>
2018-10-30 10:30:15 +0000

    pulse loading animation (with logo) added as its own class

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -7,6 +7,7 @@ import PublicProfile from "./PublicProfile";
 import Settings from "./Settings";
 import PasswordReset from "./PasswordReset";
 import Update from "./Update";
+import Loading from "./Loading";
 import axios from 'axios';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import { Redirect } from 'react-router-dom'
@@ -277,6 +278,7 @@ console.log("Error resetting password");
                 <Route path="/publicProfile" render={(props) =>  <PublicProfile data={this.state.data} loggedInAs={this.state.loggedInAs} />} />
                 <Route path="/updateReach" render={(props) =>  <Update data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} token_to_pass_on = {this.state.token_to_pass_on} handleLoginFromRegistrationSubmit = {this.handleLoginFromRegistrationSubmit} />} />
                 <Route path="/settings" render={(props) =>  <Settings data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
+                <Route path="/loading" render={(props) =>  <Loading data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
 
 
               </React.Fragment>
@@ -314,6 +316,7 @@ console.log("Error resetting password");
           <Route path="/publicprofile" render={(props) =>  <Profile data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
           <Route path="/updateReach" render={(props) =>  <Update data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} handleLoginFromRegistrationSubmit = {this.handleLoginFromRegistrationSubmit} />} />
           <Route path="/settings" render={(props) =>  <Settings data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
+          <Route path="/loading" render={(props) =>  <Loading data={this.state.data} loggedInAs={this.state.loggedInAs} login= {this.state.login} />} />
 
         </React.Fragment>
       </Router>
