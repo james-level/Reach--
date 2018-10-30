6a368d7244f6013693ef437809d813c80829019d
David A Pears <davidapears@gmail.com>
2018-10-30 16:41:29 +0000

    Updates layout and adds function to user-area page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -16,7 +16,7 @@ class Navbar extends Component {
     return(
       <ul className="nav-bar">
         <li>
-          <Link to="/results"><img src="/images/app_images/Reach.png" alt="home icon" width="33" height="33"></img></Link>
+          <Link to="/results"><img src="/images/app_images/pushbutton.svg" alt="home icon" width="30" height="30"></img></Link>
         </li>
         <li>
           <Link to="/usersection"><img src="/images/app_images/image.png" alt="home icon" width="25" height="25"></img></Link>
