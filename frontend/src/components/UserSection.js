4d03c1668fd6e7b91bbbb100c4e6a11a4a1f45d6
jah1603 <hendersonjamesalexander@gmail.com>
2018-10-30 12:53:52 +0000

    adds username and total reach to user section card
    Co-authored-by: David A. Pears <DavidAPears@users.noreply.github.com>
    Co-authored-by: Stephen Rooney <SFR1981@users.noreply.github.com>

--- frontend/src/components/UserSection.js
+++ frontend/src/components/UserSection.js
@@ -13,88 +13,36 @@ class UserSection extends Component {
 
   }
 
+animate(){
 
-//   animateCard(){
-//     $(window).on('load',function(){
-//   $('#wrapper').addClass('loaded');
-// })
-//
-// $('.more-info').click(function(){
-//   $("#card").toggleClass('flip');
-//   $('#arrow').remove();
-// });
-// $('#background').click(function(){
-//   $('#card').removeClass('flip');
-// })
-//   }
+  $(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+
+}
+
+total_reach(instagram_followers, twitter_followers, youtube_followers){return instagram_followers + twitter_followers + youtube_followers}
+
 
 
 render(){
 
-  // this.animateCard()
+  this.animate()
 
   return (
 
-    <div id="wrapper">
-  <div id="content">
-    <div id="card">
-      <div id="front">
-        <div id="arrow"><i class="fa fa-arrow-left"></i></div>
-        <div id="top-pic"></div>
-        <div id="avatar"></div>
-        <div id="info-box">
-          <div class="info">
-            <h1>Jesse Couch</h1>
-            <h2>Digital Creative</h2>
-          </div>
-        </div>
-        <div id="social-bar">
-          <a href="https://www.facebook.com/designcouch" target="_blank">
-            <i class="fa fa-facebook"></i>
-          </a>
-          <a href="https://www.twitter.com/designcouch" target="_blank">
-          <i class="fa fa-twitter"></i>
-          </a>
-          <a href="https://www.dribbble.com/designcouch" target="_blank">
-          <i class="fa fa-dribbble"></i>
-          </a>
-          <a href="https://www.codepen.io/designcouch/public">
-          <i class="fa fa-codepen"></i>
-          </a>
+    <div className="user_card">
 
-          <a href="javascript:void" class="more-info">
-            <i class="fa fa-user"></i>
-          </a>
-        </div>
-      </div>
-      <div id="back">
-        <div class="back-info">
-          <p>My name is Jesse Couch, and I am an award winning, intensely creative, coffee-fueled front-end web designer and developer. My style and approach are very straight-forward — I obsess about keeping things as simple as humanly possible. That's it. If you like bells and whistles for the sake of bells and whistles, look elsewhere - but if you want to remain laser-focused on the goals for your new website, it's time time to talk.</p>
-        </div>
-        <div id="social-bar">
-          <a href="https://www.facebook.com/designcouch" target="_blank">
-            <i class="fa fa-facebook"></i>
-          </a>
-          <a href="https://www.twitter.com/designcouch" target="_blank">
-          <i class="fa fa-twitter"></i>
-          </a>
-          <a href="https://www.dribbble.com/designcouch" target="_blank">
-          <i class="fa fa-dribbble"></i>
-          </a>
-          <a href="https://www.codepen.io/designcouch/public">
-          <i class="fa fa-codepen"></i>
-          </a>
-          <a href="javascript:void" class="more-info">
-            <i class="fa fa-undo"></i>
-          </a>
-        </div>
-      </div>
+    <figure class="snip1344"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample1.jpg" alt="profile-sample1" class="background"/><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample1.jpg" alt="profile-sample1" class="profile"/>
+  <figcaption>
+    <h3>{this.props.loggedInAs}<span>Reach: {this.total_reach(this.props.data.instagram_followers, this.props.data.twitter_followers, this.props.data.youtube_followers)}</span></h3>
+    <div class="icons"><a href="#"><i class="ion-social-reddit-outline"></i></a><a href="#"> <i class="ion-social-twitter-outline"></i></a><a href="#"> <i class="ion-social-vimeo-outline"></i></a></div>
+  </figcaption>
+</figure>
+
     </div>
-    <div id="background">
-      <div id="background-image"></div>
-    </div>
-  </div>
-</div>
   )
 
 
