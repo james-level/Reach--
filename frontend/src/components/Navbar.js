f3e55386a9499fb281fefc3055dd4aec8b9a1ef9
David A Pears <davidapears@gmail.com>
2018-10-31 14:35:19 +0000

    Adds pulse effect to main 'Reach' button in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,17 +14,25 @@ class Navbar extends Component {
     console.log(this.props);
 
     return(
+      <div className="reach_table">
       <ul className="nav-bar">
         <li>
-          <Link to="/usersection"><img src="/images/app_images/image.png" alt="home icon" width="25" height="25"></img></Link>
+          <Link to="/usersection"><img src="/images/app_images/image.png" alt="home icon" width="35" height="35"></img></Link>
         </li>
         <li>
-          <Link to="/results"><img src="/images/app_images/pushbutton.svg" alt="home icon" width="30" height="30"></img></Link>
+          <Link to="/results">
+          <div class="circles"><img src="./images/app_images/pushbutton.svg" width="35" height="35"/>
+            <div class="circle1"><div></div></div>
+            <div class="circle2"><div></div>
+            </div>
+          </div>
+          </Link>
         </li>
         <li>
-          <Link to="/" onClick={this.props.logout}><img src="/images/app_images/signout.png" alt="home icon" width="25" height="25"></img></Link>
+          <Link to="/" onClick={this.props.logout}><img src="/images/app_images/signout.png" alt="home icon" width="35" height="35"></img></Link>
         </li>
       </ul>
+    </div>
 
     )
   }
