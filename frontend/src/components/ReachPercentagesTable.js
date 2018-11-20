b996614bd801a795b19cb7dd1f731b27b960cd4d
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-20 12:38:19 +0000

    percentages table added with props

--- frontend/src/components/ReachPercentagesTable.js
+++ frontend/src/components/ReachPercentagesTable.js
@@ -1,6 +1,7 @@
 import React from "react";
+import Profile from './Profile';
 
-const ReachPercentagesTable = ({ direction, clickFunction, glyph }) => (
+const ReachPercentagesTable = () => (
 
   <div className="reach-stats">
   <div className="reach_table">
@@ -8,15 +9,15 @@ const ReachPercentagesTable = ({ direction, clickFunction, glyph }) => (
       <li>
         {/* <p class="youtube os scnd-font-color">Youtube</p> */}
         <p class="youtube os scnd-font-color"><img src="../images/app_images/youtube-icon.png" height="30" width="30"></img></p>
-        <p class="os-percentage">{Math.floor((100/this.total_reach()) * this.youtube_followers())}<sup>%</sup></p>
+        <p class="os-percentage">{Math.floor((100/this.props.total_reach()) * this.props.youtube_followers())}<sup>%</sup></p>
       </li>
       <li>
         <p class="twitter os scnd-font-color"><img src="../images/app_images/twitter-icon.png" height="30" width="30"></img></p>
-        <p class="os-percentage">{Math.floor((100/this.total_reach()) * this.twitter_followers())}<sup>%</sup></p>
+        <p class="os-percentage">{Math.floor((100/this.props.total_reach()) * this.props.twitter_followers())}<sup>%</sup></p>
       </li>
       <li>
         <p class="instagram os scnd-font-color"><img src="../images/app_images/instagram-icon.png" height="30" width="30"></img></p>
-        <p class="os-percentage">{Math.floor((100/this.total_reach()) * this.instagram_followers())}<sup>%</sup></p>
+        <p class="os-percentage">{Math.floor((100/this.props.total_reach()) * this.props.instagram_followers())}<sup>%</sup></p>
       </li>
       <li>
         <p class="facebook os scnd-font-color"><img src="../images/app_images/facebook-icon.png" height="30" width="30"></img></p>
