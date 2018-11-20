f7f052b8c5cfac742c31e1e77e87a5569e1a3569
David A Pears <davidapears@gmail.com>
2018-11-20 12:24:30 +0000

    Messaging styled a little

--- frontend/src/components/ChatApp.js
+++ frontend/src/components/ChatApp.js
@@ -45,7 +45,7 @@ class ChatApp extends React.Component {
   render() {
     return (
       <div className="container">
-        <h3>Reach Chat</h3>
+        <h3 className="chat">Reach Chat...slide into those DM's</h3>
         <Messages messages={this.state.messages} />
         <ChatInput onSend={this.sendHandler} />
       </div>
