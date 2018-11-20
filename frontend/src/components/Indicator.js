dd24725cf0f5909be5199a333427c6beed0e7da4
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-20 12:52:55 +0000

    create indicator component

---
+++ frontend/src/components/Indicator.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Indicator = () => (
+
+  <div class="loader">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+
+);
+
+export default Indicator;
